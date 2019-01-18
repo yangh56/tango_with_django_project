bb3c6eef8c553fa4f7ddbf60e3fe6c3dcf96961e
Zhouyang Shen <2359009s@student.gla.ac.uk>
2019-01-18 17:25:47 +0000

    The end of chapter 8

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -39,7 +39,7 @@ def add_page(request, category_name_slug):
         form = PageForm(request.POST)
         if form.is_valid():
             if category:
-                page = form.save(commit=False)
+                page = form.save(commit=True)
                 page.category = category
                 page.views = 0
                 page.save()
