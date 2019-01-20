d69ae39bcbe8dcea30587d52914ccc868f119810
Zhouyang Shen <2359009s@student.gla.ac.uk>
2019-01-20 16:25:38 +0000

    End of the chapter 10

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,STATIC_DIR,MEDIA_ROOT,],
+        'DIRS': [TEMPLATE_DIR,STATIC_DIR,MEDIA_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
