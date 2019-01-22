4ae91f9cb1bbe62c6e115df5f1b8ca2f78550b13
Zhouyang Shen <2359009s@student.gla.ac.uk>
2019-01-22 21:49:00 +0000

    o

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -45,7 +45,7 @@ def index(request):
     return response
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <a href=' '>Index</a >") 
+    return HttpResponse("Rango says here is the about page <a href='/rango/'>Index</a >") 
 
 
 
