9a2a00e4bcb1225ffcf58e61c4d094d04e06b53b
Zhouyang Shen <2359009s@student.gla.ac.uk>
2019-01-22 12:07:39 +0000

    Commit

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -24,6 +24,5 @@ app_name = 'rango'
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^rango/',include('rango.urls')),
-    url(r'^about/',include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
