13089a307ed49ad15bce217f0c7ba830d0e9ab83
Zhouyang Shen <2359009s@student.gla.ac.uk>
2019-01-23 00:58:26 +0000

    last commit

--- rango/views.py
+++ rango/views.py
@@ -45,7 +45,15 @@ def index(request):
     return response
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <a href='/rango/'>Index</a >") 
+    request.session.set_test_cookie()
+    context_dict = {}
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request,'rango/about.html',context=context_dict)
+    return response
 
 
 
